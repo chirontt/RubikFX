1e79e9c81e9cf2dc0207197ee91ee029bb27770e
Tue Ton <49886739+chirontt@users.noreply.github.com>
2022-02-15 19:57:17 -0500

    generate jlink image for the application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module rubikfx {
     requires javafx.controls;
-	requires transitive javafx.graphics;
+    requires transitive javafx.graphics;
     requires org.fxyz3d.importers;
 
-	opens com.jpl.games to javafx.graphics;
+    opens com.jpl.games to javafx.graphics;
 }
